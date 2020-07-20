207281fd660cbacd3b52165d022d0dddb74ee493
singhvikash630 <singh.vikash630@gmail.com>
2020-07-20 13:07:56 +0530

    code changes for circuit breaker functionality.

--- homeloan-client/src/main/java/com/eureka/homeloan/service/HomeLoanService.java
+++ homeloan-client/src/main/java/com/eureka/homeloan/service/HomeLoanService.java
@@ -1,10 +1,13 @@
 package com.eureka.homeloan.service;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.eureka.homeloan.dao.HomeLoanRepositry;
 import com.eureka.homeloan.entity.LoanType;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @Service
 public class HomeLoanService {
@@ -16,8 +19,18 @@ public class HomeLoanService {
 		return repositry.findById(id).orElse(null);
 	}
 	
+	@HystrixCommand(fallbackMethod = "saveLoanType_Fallbacks")
 	public LoanType saveLoanType(LoanType type) {
 		return repositry.save(type);
+	}
+	
+	@SuppressWarnings("unused")
+	private String saveLoanType_Fallbacks(LoanType type) {
+
+		System.out.println("HomeLoan Service is down!!! fallback route enabled...");
+
+		return "CIRCUIT BREAKER ENABLED!!! No Response From HomeLoan Service at this moment. "
+				+ " Service will be back shortly - " + new Date();
 	}
 
 }
